7e21d39bf4a824ca1924d31f7104ccc286dc07a0
42MARTIN42 <98013991+42MARTIN42@users.noreply.github.com>
2023-04-13 19:15:04 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
 Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(338.92, -12.86, 153.85, 156.43) -- Create preview ped at these coordinates
+Config.PedCoords = vector4(338.76, -27.04, 158.12, 167.01) -- Create preview ped at these coordinates
 Config.HiddenCoords = vector4(339.45, -40.26, 153.29, 149.12) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(338.23, -14.68, 154.49, 340.71) -- Camera coordinates for character preview screen vector3(364.62, -18.69, 153.29)
+Config.CamCoords = vector4(338.310455, -28.988819, 158.720001, 167.01+180.0) -- Camera coordinates for character preview screen vector3(364.62, -18.69, 153.29)
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 Config.customNationality = false -- Defines if Nationality input is custom of blocked to the list of Countries
 
@@ -15,11 +15,12 @@ Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characte
 Config.FivemAppearance = true -- True = iLLeniumStudios's fivem apperance/ False = basic qb-clothing 
 
 --Cam Config
-Config.MinDOF = 1.7 --needs to be floot
-Config.MaxDOF = 2.5 
-Config.BlurStrengt = 0.20
-Config.POV = 25.3
-Config.CamRoll = 0.0
+Config.CameraFoV = 25.0 -- Creates a variable called CameraFoV and sets it to the field of view of the camera
+Config.CameraNearDof = 0.3 -- Creates a variable called CameraNearDof and sets it to the near dof of the camera
+Config.CameraFarDof = 10.0 -- Creates a variable called CameraFarDof and sets it to the far dof of the camera
+Config.CameraDofStrength = -10.0 -- Creates a variable called CameraDofStrength and sets it to the dof strength of the camera
+Config.ShakeType = "HAND_SHAKE" -- Creates a variable called ShakeType and sets it to the shake type of the camera
+Config.CameraShake = 0.2 -- Creates a variable called CameraShake and sets it to the shake of the camera
 
 --Anim config
 Config.RandomAnims = {     
